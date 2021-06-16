b71cf1b15276cee77b85cbe78f0bb1d8cf14fff2
--global <riouga2020@gmail.com>
2021-06-17 01:53:57 +0900

    bootstrapのform input用のコンポーネント作成

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-var TestComp = {template:"<div><slot>ゲスト</slot>さんようこそ!!</div>"}
+var bootFormInput = 
+	{
+		template:
+		`<div class="form-group">
+			<label>{{cols}}</label>
+			<input type="text" class="form-control w-25"/>
+		</div>`,
+		props:['cols'],
+	}
 
 new Vue({
-    el: '#app',
-    components:{
-        'test-comp':TestComp
-    }
+	el: '#app',
+	data:function(){
+		return {
+			inputDataList:[
+					"氏名",
+					"郵便番号",
+					"電話番号"
+				]
+		}
+	} ,
+	components: {
+		'boot-form-input': bootFormInput
+	}
 })
